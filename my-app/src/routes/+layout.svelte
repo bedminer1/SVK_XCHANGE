6377c75959d96af79c422cc830aa48bb3a516ff4
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-02-18 00:22:06 +0800

    standard styling done

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+    import '$lib/global.css'
+    import Footer from '$lib/Footer.svelte';
+</script>
+
+<div class="h-screen">
+    <slot />
+    <Footer />
+</div>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -5,25 +5,31 @@
     $: baseCurrency = data.baseCurrency
     $: targetCurrency = data.targetCurrency
 
-    let amount = 100
+    let amount
     $: calculatedAmount =(amount * parseFloat(exchangeData.data[targetCurrency].value)).toFixed(2)
 </script>
 
-<div>
-    <form method="get" action="/">
-        <input type="text" name="baseCurrency" placeholder="Base Currency" class="px-2 text-gray-700"/>
-        <input type="text" name="targetCurrency" placeholder="Target Currency" class="px-2 text-gray-700"/>
-        <input type="numeric" name="amount" placeholder="Amount" class="px-2 text-gray-700" bind:value={amount}/>
-        <button type="submit">Search</button>
-    </form>
-</div>
 
-<div>
-    {#if !exchangeData}
-    <p>No data yet</p>
-    {:else}
-        <h1>{data.baseCurrency} to {data.targetCurrency}</h1>
-        <h4>{exchangeData.data[targetCurrency].value.toFixed(4)} {data.targetCurrency}/{data.baseCurrency}</h4>
-        <h4>{amount}{baseCurrency} converts to {calculatedAmount} {targetCurrency}</h4>
-    {/if}
+<div class='flex flex-col justify-center items-center h-[80vh] gap-10'>
+    <h1 class='text-5xl'>CURRENCY EXCHANGER</h1>
+   <div class="text-2xl">
+        <form method="get" action="/" class="flex flex-col items-center justify-center gap-2">
+            <input type="text" name="baseCurrency" placeholder="Base Currency" class="px-2 text-gray-700"/>
+            <input type="text" name="targetCurrency" placeholder="Target Currency" class="px-2 text-gray-700"/>
+            <input type="numeric" name="amount" placeholder="Amount" class="px-2 text-gray-700" bind:value={amount}/>
+            <button type="submit">Search</button>
+        </form>
+    </div>
+
+    <div class="text-3xl text-center">
+        {#if !exchangeData}
+        <p>No data yet</p>
+        {:else}
+            <h1>{data.baseCurrency} to {data.targetCurrency}</h1>
+            <h4>{exchangeData.data[targetCurrency].value.toFixed(4)} {data.targetCurrency}/{data.baseCurrency}</h4>
+            {#if amount}
+            <h4>{amount}{baseCurrency} converts to {calculatedAmount} {targetCurrency}</h4>
+            {/if}
+        {/if}
+    </div> 
 </div>
