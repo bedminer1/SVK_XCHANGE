2a427ff30b0fbee4be91f58aceaa49cb3b9e5600
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-02-17 22:38:38 +0800

    added amount(in base currency)to be converted

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -5,12 +5,16 @@
 
     $: baseCurrency = data.baseCurrency
     $: targetCurrency = data.targetCurrency
+
+    let amount
+    $: calculatedAmount =(amount * parseFloat(exchangeData.data[targetCurrency].value)).toFixed(2)
 </script>
 
 <div>
     <form method="get" action="/">
         <input type="text" name="baseCurrency" placeholder="Base Currency" class="px-2 text-gray-700"/>
         <input type="text" name="targetCurrency" placeholder="Target Currency" class="px-2 text-gray-700"/>
+        <input type="numeric" name="amount" placeholder="Amount" class="px-2 text-gray-700" bind:value={amount}/>
         <button type="submit">Search</button>
     </form>
 </div>
@@ -21,5 +25,6 @@
     {:else}
         <h1>{data.baseCurrency} to {data.targetCurrency}</h1>
         <h4>{exchangeData.data[targetCurrency].value} {data.targetCurrency}/{data.baseCurrency}</h4>
+        <h4>{amount}{baseCurrency} converts to {calculatedAmount} {targetCurrency}</h4>
     {/if}
 </div>
